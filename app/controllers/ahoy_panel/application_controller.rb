42e31b829460a5d3645ca39080e8b2a4f5c315f7
Chris Jeon <chris@typefast.co>
2025-03-28 12:44:51 +0900

    Much simpler api key auth

--- app/controllers/ahoy_panel/application_controller.rb
+++ app/controllers/ahoy_panel/application_controller.rb
@@ -5,21 +5,9 @@ module AhoyPanel
     protected
 
     def authenticate_api_key!
-      api_key = request.headers["Ahoy-Panel-Api-Key"]
+      api_key = request.headers["Ahoy-Panel-Api-Key"].remove("Bearer").squish
 
-      return true if Rails.env.development?
-
-      ahoy_panel_root_url = "https://www.ahoypanel.com"
-
-      client = Faraday.new(url: ahoy_panel_root_url) do |conn|
-        conn.headers["Ahoy-Panel-Api-Key"] = "#{api_key}"
-        conn.request :json
-        conn.response :json
-      end
-
-      if !response.body["verified"]
-        raise
-      end
+      raise if api_key != ::AhoyPanel.config.api_key
     end
   end
 end
