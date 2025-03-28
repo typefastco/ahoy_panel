ccc293f0279103f6c6ce311ad87fa3909aa09585
Chris Jeon <chris@typefast.co>
2025-03-28 19:08:24 +0900

    Add beginning_of_day and end_of_day

--- app/controllers/ahoy_panel/views_data_controller.rb
+++ app/controllers/ahoy_panel/views_data_controller.rb
@@ -1,8 +1,8 @@
 module AhoyPanel
   class ViewsDataController < ApplicationController
     def index
-      @start_date = params[:start_date].to_date
-      @end_date = params[:end_date].to_date
+      @start_date = params[:start_date].to_date.beginning_of_day
+      @end_date = params[:end_date].to_date.end_of_day
 
       @visits = ::Ahoy::Visit.where(started_at: @start_date..@end_date)
 
