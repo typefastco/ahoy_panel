53dce1d541f0b11ba73b002cab113836cd01d69e
Chris Jeon <chris@typefast.co>
2025-04-01 08:51:40 +0900

    Modification on how the date range is built if there is no end_date

--- app/controllers/ahoy_panel/events_controller.rb
+++ app/controllers/ahoy_panel/events_controller.rb
@@ -2,10 +2,16 @@ module AhoyPanel
   class EventsController < ApplicationController
     def index
       @start_date = params[:start_date].to_date.beginning_of_day
-      @end_date = params[:end_date].to_date.end_of_day
+      @end_date = params[:end_date].blank? ? nil : params[:end_date].to_date.end_of_day
       @page = params[:page]
 
-      @events = ::Ahoy::Event.where(time: @start_date..@end_date).order(:id)
+      @date_range = if @end_date.blank?
+                      @start_date..params[:start_date].to_date.end_of_day
+                    else
+                      @start_date..@end_date
+                    end
+
+      @events = ::Ahoy::Event.where(time: @date_range).order(:id)
 
       if @page.present?
         @events = @events.page(@page)
--- app/controllers/ahoy_panel/visits_controller.rb
+++ app/controllers/ahoy_panel/visits_controller.rb
@@ -2,10 +2,16 @@ module AhoyPanel
   class VisitsController < ApplicationController
     def index
       @start_date = params[:start_date].to_date.beginning_of_day
-      @end_date = params[:end_date].to_date.end_of_day
+      @end_date = params[:end_date].blank? ? nil : params[:end_date].to_date.end_of_day
       @page = params[:page]
 
-      @visits = ::Ahoy::Visit.where(started_at: @start_date..@end_date).order(:id)
+      @date_range = if @end_date.blank?
+                      @start_date..params[:start_date].to_date.end_of_day
+                    else
+                      @start_date..@end_date
+                    end
+
+      @visits = ::Ahoy::Visit.where(started_at: @date_range).order(:id)
 
       if @page.present?
         @visits = @visits.page(@page)
