1ff508e0fa9fca3c4c33097228b846aa37bd46ab
Chris Jeon <chris@typefast.co>
2025-04-01 10:51:44 +0900

    visit filtering params

--- app/controllers/ahoy_panel/visits_controller.rb
+++ app/controllers/ahoy_panel/visits_controller.rb
@@ -4,6 +4,7 @@ module AhoyPanel
       @start_date = params[:start_date].to_date.beginning_of_day
       @end_date = params[:end_date].blank? ? nil : params[:end_date].to_date.end_of_day
       @page = params[:page]
+      @filtering_params = params[:filtering_params]
 
       @date_range = if @end_date.blank?
                       @start_date..params[:start_date].to_date.end_of_day
@@ -12,6 +13,9 @@ module AhoyPanel
                     end
 
       @visits = ::Ahoy::Visit.where(started_at: @date_range).order(:id)
+      filtering_params.each do |key, value|
+        @visits = @visits.where("#{key} ilike ?", "%#{value.downcase}%")
+      end
 
       if @page.present?
         @visits = @visits.page(@page)
