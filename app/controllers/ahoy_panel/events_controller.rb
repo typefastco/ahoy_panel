c400f6ac5f61e96fad6505502e00a4b73c6f3b14
Chris Jeon <chris@typefast.co>
2025-04-01 13:35:54 +0900

    Filtering params for events

--- app/controllers/ahoy_panel/events_controller.rb
+++ app/controllers/ahoy_panel/events_controller.rb
@@ -4,6 +4,7 @@ module AhoyPanel
       @start_date = params[:start_date].to_date.beginning_of_day
       @end_date = params[:end_date].blank? ? nil : params[:end_date].to_date.end_of_day
       @page = params[:page]
+      @filtering_params = JSON.parse(params[:filtering_params], symbolize_names: true)
 
       @date_range = if @end_date.blank?
                       @start_date..params[:start_date].to_date.end_of_day
@@ -12,6 +13,19 @@ module AhoyPanel
                     end
 
       @events = ::Ahoy::Event.where(time: @date_range).order(:id)
+
+      @filtering_params.each do |key, value|
+        case key
+        when :id
+          @events = @events.where(id: value.to_i)
+        when :visit_id
+          @events = @events.where(visit_id: value.to_i)
+        when :user_id
+          @events = @events.where(user_id: value.to_i)
+        when :name
+          @events = @events.where("name ilike ?", "%#{value}%")
+        end
+      end
 
       if @page.present?
         @events = @events.page(@page)
