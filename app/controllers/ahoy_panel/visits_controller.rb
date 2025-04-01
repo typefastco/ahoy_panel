f8f8e902787e8e28ea744e44ec682a21b7965ba6
Chris Jeon <chris@typefast.co>
2025-04-01 11:33:38 +0900

    parse

--- app/controllers/ahoy_panel/visits_controller.rb
+++ app/controllers/ahoy_panel/visits_controller.rb
@@ -4,7 +4,7 @@ module AhoyPanel
       @start_date = params[:start_date].to_date.beginning_of_day
       @end_date = params[:end_date].blank? ? nil : params[:end_date].to_date.end_of_day
       @page = params[:page]
-      @filtering_params = params[:filtering_params]
+      @filtering_params = JSON.parse(params[:filtering_params], symbolize_names: true)
 
       @date_range = if @end_date.blank?
                       @start_date..params[:start_date].to_date.end_of_day
